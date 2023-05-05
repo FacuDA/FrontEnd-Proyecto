5d693f2323494eaf7e42f4727f15e7f57351a35e
FacuDA <facundoaguirre9488@gmail.com>
2023-05-05 14:29:26 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HasComponent } from './components/has/has.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkilComponent } from './components/has/edit-skill.component';
+import { NewSkillComponent } from './components/has/new-skill.component';
+import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
+
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HasComponent,
+    ProyectoComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    EditSkilComponent,
+    NewSkillComponent,
+    EditAcercaDeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    FormsModule,
+
+  ],
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/s-experiencia.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Experiencia } from '../model/experiencia';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SExperienciaService {
+  URL = environment.URL +'explab/'
+
+  constructor(private httClient: HttpClient) { }
+
+  public lista (): Observable<Experiencia[]>{
+
+    return this.httClient.get<Experiencia[]>(this.URL + 'lista');
+  }
+
+  public detail(id:number): Observable<Experiencia>{
+    return this.httClient.get<Experiencia>(this.URL + `detail/${id}`)
+  }
+
+  public save(experiencia: Experiencia): Observable<any>{
+    return this.httClient.post<any>(this.URL + 'create', experiencia);
+  }
+
+  public update(id: number,experiencia: Experiencia ):Observable<any>{
+    return this.httClient.put<any>(this.URL + `update/${id}`, experiencia);
+  }
+  public delete(id: number): Observable<any>{
+    return this.httClient.delete<any>(this.URL+ `delete/${id}`);
+  }
+}
